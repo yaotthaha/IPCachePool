dc8c2d2b618efcd0dfaf75e502319c4c14c2ea73
yaotthaha-vscode <95022881+yaotthaha@users.noreply.github.com>
2022-05-17 20:21:17 +0800

    Fix ipset.go

---
+++ ipset/ipset_other.go
@@ -0,0 +1,56 @@
+//go:build !linux
+
+package ipset
+
+import (
+	"errors"
+	"net/netip"
+)
+
+func Check() error {
+	return errors.New("not support")
+}
+
+func Create(setName string, setType string) error {
+	return errors.New("not support")
+}
+
+func AddAddr(setName string, ip netip.Addr) error {
+	return errors.New("not support")
+}
+
+func AddPrefix(setName string, cidr netip.Prefix) error {
+	return errors.New("not support")
+}
+
+func DelAddr(setName string, ip netip.Addr) error {
+	return errors.New("not support")
+}
+
+func DelPrefix(setName string, cidr netip.Prefix) error {
+	return errors.New("not support")
+}
+
+func ExistAddr(setName string, ip netip.Addr) bool {
+	return false
+}
+
+func ExistPrefix(setName string, cidr netip.Prefix) bool {
+	return false
+}
+
+func CheckAndAddAddr(setName string, ip netip.Addr) error {
+	return errors.New("not support")
+}
+
+func CheckAndAddPrefix(setName string, cidr netip.Prefix) error {
+	return errors.New("not support")
+}
+
+func CheckAndDelAddr(setName string, ip netip.Addr) error {
+	return errors.New("not support")
+}
+
+func CheckAndDelPrefix(setName string, cidr netip.Prefix) error {
+	return errors.New("not support")
+}
